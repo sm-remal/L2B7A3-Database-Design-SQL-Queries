312aa8e4de4c8d2f656a4993a646c9c973c858b1
sm-remal <remal.mozzem@gmail.com>
2026-07-04 20:33:59 +0600

    Query - 2

--- QUERY.sql
+++ QUERY.sql
@@ -66,3 +66,10 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 SELECT match_id, fixture, base_ticket_price FROM Matches 
 WHERE tournament_category = 'Champions League' AND match_status = 'Available'
+
+
+
+-- QUERY 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+SELECT user_id, full_name, email FROM Users
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%'
