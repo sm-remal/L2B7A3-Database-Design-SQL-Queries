79d5cbc2aeb90c123fd882ef30c1477ae314d272
sm-remal <remal.mozzem@gmail.com>
2026-07-04 20:27:35 +0600

    Create Bookings Table and Data Insert

--- QUERY.sql
+++ QUERY.sql
@@ -35,3 +35,23 @@ INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, 
 (103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
 (104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
 (105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
+
+
+
+-- Bookings Table
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT REFERENCES Users(user_id) NOT NULL,
+    match_id INT REFERENCES Matches(match_id) NOT NULL,
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(20) CHECK (payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost NUMERIC(10,2) CHECK (total_cost >= 0) NOT NULL
+);
+
+
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL,  NULL, 150.00),
+(505, 3, 102, 'C-20', 'Pending', 120.00);
