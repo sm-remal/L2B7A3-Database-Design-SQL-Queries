7370098edbb27192145fb361828b232ddb143bb4
sm-remal <remal.mozzem@gmail.com>
2026-07-04 20:26:14 +0600

    Create Match Table and Data Insert

--- QUERY.sql
+++ QUERY.sql
@@ -16,3 +16,22 @@ INSERT INTO Users (full_name, email, role, phone_number) VALUES
 ('Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
 ('Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 ('Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL); 
+
+
+
+-- Match Table
+CREATE TABLE Matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(150) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price DECIMAL (10,2) CHECK (base_ticket_price >= 0) NOT NULL,
+    match_status VARCHAR(50) CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed')) NOT NULL
+);
+
+
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
